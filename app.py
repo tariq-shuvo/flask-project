4f79c5a485de3b3badca7f1638bede0db6a87278
tariq-shuvo <tariq10043@gmail.com>
2022-05-18 12:50:31 +0600

    variable rules

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
+@app.route("/hello/<name>")
+def hello_name(name):
+    return "Hello %s!" %name
+
+@app.route("/blog/<int:postID>")
+def blog(postID):
+    return "Blog number is %d!" %postID
+
+@app.route("/rev/<float:revNo>")
+def revision(revNo):
+    return "Revision number is %f!" %revNo
+
 if __name__ == '__main__':
     app.run(debug=True)
