b84a2e75efd485cd1d0b736bc688b95f480b0d13
tariq-shuvo <tariq10043@gmail.com>
2022-05-18 14:51:23 +0600

    URL Building

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello world"
+
+@app.route("/admin")
+def hello_admin():
+    return "Hello Admin"
 
 @app.route("/hello/<name>")
 def hello_name(name):
@@ -16,6 +20,13 @@ def blog(postID):
 @app.route("/rev/<float:revNo>")
 def revision(revNo):
     return "Revision number is %f!" %revNo
+
+@app.route("/redirect/<name>")
+def redirect_admin(name):
+    if name == "admin":
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_name', name=name))
 
 if __name__ == '__main__':
     app.run(debug=True)
